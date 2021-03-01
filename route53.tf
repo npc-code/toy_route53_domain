725b821025576178fcdebaa5a0baf59e479dd6d2
Nic <nic.ceynowa@gmail.com>
2021-03-01 14:41:47 -0500

    going to strip this down to just the zone, worry about creating a certificate specifically for one subdomain

--- route53.tf
+++ route53.tf
@@ -5,36 +5,36 @@ resource "aws_route53_zone" "main_zone" {
 
 #maybe move this to another module?
 
-resource "aws_acm_certificate" "cert_request" {
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method         = "DNS"
+#resource "aws_acm_certificate" "cert_request" {
+#  domain_name               = var.domain_name
+#  subject_alternative_names = ["*.${var.domain_name}"]
+#  validation_method         = "DNS"
 
-  tags = {
-    Name : var.domain_name
-  }
+#  tags = {
+#    Name : var.domain_name
+#  }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
 
-resource "aws_route53_record" "validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert_request.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+#resource "aws_route53_record" "validation_record" {
+#  for_each = {
+#    for dvo in aws_acm_certificate.cert_request.domain_validation_options : dvo.domain_name => {
+#      name   = dvo.resource_record_name
+#      record = dvo.resource_record_value
+#      type   = dvo.resource_record_type
+#    }
+#  }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = var.ttl
-  type            = each.value.type
-  zone_id         = aws_route53_zone.main_zone.zone_id
-}
+#  allow_overwrite = true
+#  name            = each.value.name
+#  records         = [each.value.record]
+#  ttl             = var.ttl
+#  type            = each.value.type
+#  zone_id         = aws_route53_zone.main_zone.zone_id
+#}
 
 #resource "aws_acm_certificate_validation" "certificate_validation" {
 #  certificate_arn         = aws_acm_certificate.cert_request.arn
